774416ca6cee30027a5b3963830654cbba4479e0
tianyu <tianyu@bianjie.ai>
2023-01-10 17:44:44 +0800

    init

---
+++ src/Exception/InvalidArgumentException.php
@@ -0,0 +1,1 @@
+<?php/** * * User: yu * Date: 2023/1/9 * Email: <Tianyu@bianjie.ai> */namespace Bianjieai\AvataSdkPhp\Exception;final class InvalidArgumentException extends \InvalidArgumentException implements ClientException{}
---
+++ src/Service/NFT.php
@@ -0,0 +1,1 @@
+<?php/** * * User: yu * Date: 2023/1/9 * Email: <Tianyu@bianjie.ai> */namespace Bianjieai\AvataSdkPhp\Service;use Bianjieai\AvataSdkPhp\BaseService;class NFT extends BaseService{}
