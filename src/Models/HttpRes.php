560d982f79ff2c4a5f0d3d9db8d31499a5120d9c
tianyu <tianyu@bianjie.ai>
2023-01-11 17:48:45 +0800

    重构基础框架和链账户创建接口

--- src/Models/HttpRes.php
+++ src/Models/HttpRes.php
@@ -1,1 +1,50 @@
-<?php/** * * User: yu * Date: 2023/1/11 * Email: <Tianyu@bianjie.ai> */namespace Bianjieai\AvataSdkPhp\Models;class HttpRes{    /**     * @var int http status code     */    private $code;    /**     * @var string http message     */    private $message;    /**     * HttpRes constructor.     * @param int $code     * @param string $message     */    public function __construct(int $code, string $message)    {        $this->code = $code;        $this->message = $message;    }    /**     * @return int 获取Code     */    public function getCode() :int    {        return $this->code;    }    /**     * @return string 获取Message     */    public function getMessage() :string    {        return $this->message;    }}
+<?php
+/**
+ *
+ * User: yu
+ * Date: 2023/1/11
+ * Email: <Tianyu@bianjie.ai>
+ */
+
+namespace Bianjieai\AvataSdkPhp\Models;
+
+
+class HttpRes
+{
+    /**
+     * @var int http status code
+     */
+    private $code;
+
+    /**
+     * @var string http message
+     */
+    private $message;
+
+    /**
+     * HttpRes constructor.
+     * @param int $code
+     * @param string $message
+     */
+    public function __construct(int $code, string $message)
+    {
+        $this->code = $code;
+        $this->message = $message;
+    }
+
+    /**
+     * @return int 获取Code
+     */
+    public function getCode() :int
+    {
+        return $this->code;
+    }
+
+    /**
+     * @return string 获取Message
+     */
+    public function getMessage() :string
+    {
+        return $this->message;
+    }
+}
