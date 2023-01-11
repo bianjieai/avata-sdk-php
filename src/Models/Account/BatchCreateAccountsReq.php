31cf13ca5fe4cc76bb9ae1cae55fb9cfe66c3b69
tianyu <tianyu@bianjie.ai>
2023-01-11 17:32:50 +0800

    重构基础框架和链账户创建接口

--- src/Models/Account/BatchCreateAccountsReq.php
+++ src/Models/Account/BatchCreateAccountsReq.php
@@ -1,1 +1,41 @@
-<?php/** * * User: yu * Date: 2023/1/10 * Email: <Tianyu@bianjie.ai> */namespace Bianjieai\AvataSdkPhp\Models\Account;use Bianjieai\AvataSdkPhp\Models\BaseRequest;class BatchCreateAccountsReq extends BaseRequest{    /**     * @var int 批量创建链账户的数量     */    public $count;    /**     * BatchCreateAccountsReq constructor.     * @param int $count     * @param string $operationID     */    public function __construct(int $count, string $operationID)    {        $this->count = $count;        $this->operationID = $operationID;    }    /**     * 返回字段的名称     *     * @return string     */    public function getCountKey() :string    {        return "count";    }}
+<?php
+/**
+ *
+ * User: yu
+ * Date: 2023/1/10
+ * Email: <Tianyu@bianjie.ai>
+ */
+
+namespace Bianjieai\AvataSdkPhp\Models\Account;
+
+use Bianjieai\AvataSdkPhp\Models\BaseRequest;
+
+class BatchCreateAccountsReq extends BaseRequest
+{
+    /**
+     * @var int 批量创建链账户的数量
+     */
+    public $count;
+
+
+    /**
+     * BatchCreateAccountsReq constructor.
+     * @param int $count
+     * @param string $operationID
+     */
+    public function __construct(int $count, string $operationID)
+    {
+        $this->count = $count;
+        $this->operationID = $operationID;
+    }
+
+    /**
+     * 返回字段的名称
+     *
+     * @return string
+     */
+    public function getCountKey() :string
+    {
+        return "count";
+    }
+}
